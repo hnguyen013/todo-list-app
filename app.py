a7e74cf6fea45c328ce4512b8e65cea874ad7141
hnguyen013 <hanhnguyen130526@gmail.com>
2025-10-29 13:57:00 +0700

    feat: status

--- app.py
+++ app.py
@@ -10,14 +10,26 @@ def list_tasks():
         print("Chưa có công việc nào trong danh sách!")
         return
     print("\nDanh sách công việc:")
-    for x in tasks:
+    for task in tasks:
         i+=1
-        if (x['completed'] == True): 
-            print(f"{i}. {x['name']} - Hoàn thành")
-        else: print(f"{i}. {x['name']} - Chưa hoàn thành")
+        if (task['completed'] == True): 
+            print(f"{i}. {task['name']} - Hoàn thành")
+        else: print(f"{i}. {task['name']} - Chưa hoàn thành")
+# Kiểm tra xem công việc hoàn thành chưa
+def complete_task():
+    print("\nTrạng thái:")
+    check = True
+    for task in tasks:
+        if (task['completed'] == False): 
+            check = False
+            break
+    if (check == True): print("Đã hoàn thành công việc!")
+    else: print("Chưa hoàn thành công việc!")
+    
 # --- Điểm bắt đầu của chương trình ---
 if __name__ == "__main__":
     print("Chào mừng đến với ứng dụng To-Do List!\n")
 add_task("Học bài Git và GitHub")
 add_task("Làm bài tập thực hành ở nhà")
 list_tasks()
+complete_task()
